7ad7bb633b58c575cdd07b2ef23007194d45be1d
adhammohamed123 <adhammo909@gmail.com>
2025-12-28 23:42:00 +0200

    alter validation behavior to handle more than one validator

--- Bookify/Bookify/Bookify.Application/Abstractions/Behaviors/ValidationBehavior.cs
+++ Bookify/Bookify/Bookify.Application/Abstractions/Behaviors/ValidationBehavior.cs
@@ -4,28 +4,83 @@ using MediatR;
 
 namespace Bookify.Application.Abstractions.Behaviors
 {
-    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    public class ValidationBehavior<TRequest, TResponse>
+        : IPipelineBehavior<TRequest, TResponse>
         where TRequest : IBaseCommand
     {
-        private readonly IValidator? validator;
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
 
-        public ValidationBehavior(IValidator? validator)
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
         {
-            this.validator = validator;
+            _validators = validators;
         }
-        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+
+        public async Task<TResponse> Handle(
+            TRequest request,
+            RequestHandlerDelegate<TResponse> next,
+            CancellationToken cancellationToken)
         {
-           if (validator is null)
-           {
-                return await next(cancellationToken);
-           }
-           var context = new ValidationContext<TRequest>(request);
-           var validationResult = validator.Validate(context);
-           if (!validationResult.IsValid)
-           {
-             throw new ValidationException(validationResult.Errors);
-           }
-           return await next(cancellationToken);
+            if (!_validators.Any())
+            {
+                return await next();
+            }
+
+            var context = new ValidationContext<TRequest>(request);
+
+            var validationErrors = _validators
+                .Select(validator => validator.Validate(context))
+                .Where(validationResult => validationResult.Errors.Any())
+                .SelectMany(validationResult => validationResult.Errors)
+                .Select(validationFailure => new ValidationError(
+                    validationFailure.PropertyName,
+                    validationFailure.ErrorMessage))
+                .ToList();
+
+            if (validationErrors.Any())
+            {
+                throw new ValidationException(validationErrors);
+            }
+
+            return await next();
         }
     }
+
+   
+
+    public sealed class ValidationException : Exception
+    {
+        public ValidationException(IEnumerable<ValidationError> errors)
+        {
+            Errors = errors;
+        }
+
+        public IEnumerable<ValidationError> Errors { get; }
+    }
+    public sealed record ValidationError(string PropertyName, string ErrorMessage);
+
+
+    //public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    //    where TRequest : IBaseCommand
+    //{
+    //    private readonly IValidator? validator;
+
+    //    public ValidationBehavior(IValidator? validator)
+    //    {
+    //        this.validator = validator;
+    //    }
+    //    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    //    {
+    //       if (validator is null)
+    //       {
+    //            return await next(cancellationToken);
+    //       }
+    //       var context = new ValidationContext<TRequest>(request);
+    //       var validationResult = validator.Validate(context);
+    //       if (!validationResult.IsValid)
+    //       {
+    //         throw new ValidationException(validationResult.Errors);
+    //       }
+    //       return await next(cancellationToken);
+    //    }
+    //}
 }
